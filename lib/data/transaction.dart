da8c3ecc1988adcbf0bce19b5afd32c037e4b157
Andrew <kungpaogao@gmail.com>
2019-08-09 01:11:10 -0400

    Updated transaction model

--- lib/data/transaction.dart
+++ lib/data/transaction.dart
@@ -1,13 +1,14 @@
 class Transaction {
-  String houseId;
+  String houseId, comment;
   DateTime dateTime;
   double totalPrice;
   List<UserAmt> people;
   String purchaser;
 
-  Transaction(String houseId, DateTime dateTime, double totalPrice,
-      List<UserAmt> people, String purchaser) {
+  Transaction(String houseId, String comment, DateTime dateTime,
+      double totalPrice, List<UserAmt> people, String purchaser) {
     this.houseId = houseId;
+    this.comment = comment;
     this.dateTime = dateTime;
     this.totalPrice = totalPrice;
     this.purchaser = purchaser;
@@ -16,7 +17,7 @@ class Transaction {
 }
 
 class UserAmt {
-  String user;
+  String user; // this is a user id
   double amt;
 
   UserAmt(String user, double amt) {
@@ -26,10 +27,11 @@ class UserAmt {
 }
 
 List<Transaction> sampleTrans = [
-  Transaction("house-1", (new DateTime(2019, 8, 8)), 21.97,
+  Transaction("house-1", "toilet paper", (new DateTime(2019, 8, 8)), 21.97,
       [UserAmt("user-1", 12.97), UserAmt("user-2", 9)], "user-1"),
   Transaction(
       "house-1",
+      "memes",
       (new DateTime(2019, 8, 8)),
       26,
       [
